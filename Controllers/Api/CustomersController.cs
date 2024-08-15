40597229b2e2b6cc9cfe567715b01cd5946b0981
Rupam Bhoyar <109217377+Infinix006@users.noreply.github.com>
2024-08-15 17:14:26 +0530

    Created API for Customers and Movies

---
+++ Controllers/Api/CustomersController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Movie_Rental_Store.Data;
+using Movie_Rental_Store.Models;
+
+namespace Movie_Rental_Store.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //Get Api/Customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        //Get Api/Customers/1 
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customers = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customers == null) 
+                return NotFound();
+
+            return Ok(customers);
+        }
+
+        //Post Api/Customers
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id),customer);
+        }
+
+
+        //PUT /Api/Customers/1
+        [HttpPut]   
+        public IHttpActionResult UpdateCustomer(int id, Customer customer)
+        {
+            if(ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var customerInDb = _context.Customers.Single(m => m.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            customerInDb.Name = customer.Name;
+            customerInDb.BirthDate = customer.BirthDate;
+            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            _context.SaveChanges();
+
+            return Ok(customerInDb);
+        }
+
+        //Delete /Api/Customers/1
+
+        [HttpDelete]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.Single(m =>m.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+
+            return Ok(customerInDb);
+        }
+    }
+}
---
+++ Controllers/Api/MoviesController.cs
@@ -0,0 +1,93 @@
+﻿using Movie_Rental_Store.Data;
+using Movie_Rental_Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Movie_Rental_Store.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET /Api/Movies
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movies.ToList();
+        }
+
+
+        //GET /Api/Movies/1
+        public IHttpActionResult GetMovies(int id)
+        {
+            var movie = _context.Movies.Single(x => x.Id == id);
+
+            if (movie == null) 
+                return NotFound();
+
+            return Ok(movie);
+        }
+
+
+        //POST /Api/Movies
+        [HttpPost]
+        public IHttpActionResult CreateMovies(Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            return Created(Request.RequestUri + "/" + movie.Id, movie);
+        }
+
+        //PUT /Api/Movies/1
+        [HttpPut]
+        public IHttpActionResult UpdateMovies(int id,  Movie movie)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var moviesInDb = _context.Movies.Single(m => m.Id == id);
+
+            if(moviesInDb == null)
+                return NotFound();
+
+            moviesInDb.Name = movie.Name;
+            moviesInDb.GenreId = movie.GenreId;
+            moviesInDb.ReleaseDate = movie.ReleaseDate;
+            moviesInDb.NoInStock = movie.NoInStock;
+
+            _context.SaveChanges();
+
+            return Ok(moviesInDb);
+        }
+
+        //DELETE /Api/Movies/1
+        [HttpDelete]
+        public IHttpActionResult DeleteMovies(int id)
+        {
+            var movieInDb = _context.Movies.Single(m => m.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+
+            return Ok(movieInDb);
+        }
+
+
+    }
+}
