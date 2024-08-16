785484eb77cd4bb0c4cfbc9309ed021003d47c6e
Rupam Bhoyar <109217377+Infinix006@users.noreply.github.com>
2024-08-16 14:40:53 +0530

    Client Side Development
    Added DataTable Plugin and made table responsive. Implemented serch, sort and pagination using datatable plugin.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,8 +8,12 @@ namespace Movie_Rental_Store
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootBox.js",
+                        "~/Scripts/DataTables/jquery.dataTables.js",
+                        "~/Scripts/DataTables/dataTables.bootstrap4.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,10 +24,12 @@ namespace Movie_Rental_Store
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                        
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap4.css", 
                       "~/Content/site.css"));
         }
     }
